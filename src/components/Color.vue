ad3c0afe159e046227032f6771291a66e0dd1928
Daniel Fornarini <daniel@monade.io>
2022-03-17 16:29:57 +0100

    add data table

---
+++ src/components/Color.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :style="{ 'width': '30px', height: '30px', 'background-color': color }"></div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from "vue-class-component";
+import {Prop} from "vue-property-decorator";
+
+@Options({})
+export default class Color extends Vue {
+  @Prop({ required: true }) person!: { user: string, role: string, color: string };
+
+  get color() {
+    return this.person.color;
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-data-table :sort="sort" @sorted="onSort" :values="value" :columns="columns" :loading="false" >
+    <template #column_color="prop">
+      <color :person="prop.value" />
+    </template>
+  </v-data-table>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {Options, Vue} from 'vue-class-component';
+import VDataTable from "@/components/VDataTable.vue";
+import {ColumnData} from "@/components/VDataTable.vue";
+import Color from "@/components/Color.vue";
+import {Watch} from "vue-property-decorator";
 
 @Options({
   components: {
-    HelloWorld,
+    VDataTable,
+    Color
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  columns: ColumnData[] = [
+    {
+      sortable: true,
+      sort: '',
+      name: 'user',
+      class: '',
+      default: '-',
+      label: 'User'
+    },
+    {
+      sortable: true,
+      sort: '',
+      name: 'role',
+      class: '',
+      default: '-',
+      label: 'Role'
+    },
+    {
+      sortable: false,
+      sort: '',
+      name: 'color',
+      class: '',
+      default: '',
+      label: 'Color'
+    }
+  ];
+
+  value: any[] = [
+    {
+      user: 'Daniel',
+      role: 'Dev',
+      color: 'red'
+    },
+    {
+      user: 'Federico',
+      role: 'Dev',
+      color: 'rebeccapurple'
+    },
+    {
+      user: 'Bea',
+      role: 'designer',
+      color: 'gray'
+    },
+    {
+      user: 'Gabbo',
+      role: 'Sales',
+      color: 'green'
+    }
+  ];
+
+  sort = '';
+
+  onSort(value: string) {
+    this.sort = value;
+  }
+
+  @Watch('sort')
+  onSortChange() {
+    if(this.sort[0] === '-') {
+      const s = this.sort.slice(1);
+      this.value.sort((a: any, b: any) => {
+        if(a[s].toLowerCase() < b[s].toLowerCase()) {
+          return 1;
+        }
+        if(a[s].toLowerCase() > b[s].toLowerCase()) {
+          return -1
+        }
+        return 0;
+      });
+    } else {
+      this.value.sort((a: any, b: any) => {
+        if(a[this.sort].toLowerCase() < b[this.sort].toLowerCase()) {
+          return -1;
+        }
+        if(a[this.sort].toLowerCase() > b[this.sort].toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+  }
+}
 </script>
